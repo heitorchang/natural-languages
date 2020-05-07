6f57fbc17d2eefc89c2bfdcd777bcf17b11c4527
Heitor Chang <heitorchang@gmail.com>
2020-05-07 15:43:50 -0300

    rename category

--- italiano/conversation_guide/src/data/order.js
+++ italiano/conversation_guide/src/data/order.js
@@ -1,4 +1,4 @@
-const categoryOrder = ['Arrival and departure', 'Communications', 'Conversation', 'Disabled travelers', 'Drinks', 'Eating out', 'Emergencies', 'Getting around', 'Going out', 'Health', 'Meals and cooking', 'Money', 'Need to know', 'Numbers', 'On the menu', 'Pharmacy', 'Places to stay', 'Police', 'Romance', 'Shopping', 'Sightseeing', 'Sport and leisure', 'Time', 'Traveling with children'];
+const categoryOrder = ['Arrival and departure', 'Communications', 'Conversation', 'Disabled travelers', 'Drinks', 'Eating out', 'Emergencies', 'Getting around', 'Going out', 'Health', 'Meals and cooking', 'Money', 'Need to know', 'Numbers', 'On the menu', 'Pharmacy', 'Places to stay', 'Police', 'Romance', 'Shopping', 'Sightseeing', 'Sport and leisure', 'Time and date', 'Traveling with children'];
 
 // Book order
 // ['Need to know', 'Numbers', 'Time', 'Arrival and departure', 'Money', 'Conversation', 'Getting around', 'Places to stay', 'Communications', 'Sightseeing', 'Shopping', 'Sport and leisure', 'Traveling with children', 'Emergencies', 'Police', 'Health', 'Pharmacy', 'Disabled travelers', 'Eating out', 'Meals and cooking', 'Drinks', 'On the menu', 'Going out', 'Romance'];
