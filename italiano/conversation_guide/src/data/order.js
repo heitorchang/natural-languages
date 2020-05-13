1a40e8147c8652f58f707c067ce0c6f4c4a1ac9f
Heitor Chang <heitorchang@gmail.com>
2020-05-13 16:53:15 -0300

    finish copying

--- italiano/conversation_guide/src/data/order.js
+++ italiano/conversation_guide/src/data/order.js
@@ -1,5 +1,10 @@
-const categoryOrder = ['Arrival and departure', 'Communications', 'Conversation', 'Disabled travelers', 'Drinks', 'Eating out', 'Emergencies', 'Getting around', 'Going out', 'Health', 'Meals and cooking', 'Money', 'Need to know', 'Numbers', 'On the menu', 'Pharmacy', 'Places to stay', 'Police', 'Romance', 'Shopping', 'Sightseeing', 'Sport and leisure', 'Time and date', 'Traveling with children'];
+const categoryOrder = ['Arrival and departure', 'Communications', 'Conversation', 'Disabled travelers', 'Drinks', 'Eating out', 'Emergencies', 'Getting around', 'Going out', 'Health', 'Meals and cooking', 'Money', 'Need to know', 'Numbers', 'Pharmacy', 'Places to stay', 'Police', 'Romance', 'Shopping', 'Sightseeing', 'Sport and leisure', 'Time and date', 'Traveling with children'];
 
+// Skipping On the menu
+
+
+// Alphabetical order
+// ['Arrival and departure', 'Communications', 'Conversation', 'Disabled travelers', 'Drinks', 'Eating out', 'Emergencies', 'Getting around', 'Going out', 'Health', 'Meals and cooking', 'Money', 'Need to know', 'Numbers', 'Pharmacy', 'Places to stay', 'Police', 'Romance', 'Shopping', 'Sightseeing', 'Sport and leisure', 'Time and date', 'Traveling with children'];
 // Book order
 // ['Need to know', 'Numbers', 'Time and date', 'Arrival and departure', 'Money', 'Conversation', 'Getting around', 'Places to stay', 'Communications', 'Sightseeing', 'Shopping', 'Sport and leisure', 'Traveling with children', 'Emergencies', 'Police', 'Health', 'Pharmacy', 'Disabled travelers', 'Eating out', 'Meals and cooking', 'Drinks', 'On the menu', 'Going out', 'Romance'];
 
